e4525a1d6dd76224d1b87f8f7593c871136b56d2
gs221 <gs221@pc2-092-l.cs.st-andrews.ac.uk>
2019-04-01 18:06:22 +0100

    changes to report

---
+++ src/build.sh
@@ -0,0 +1,10 @@
+function exe() {
+	javac $1*.java
+}
+
+exe alphacraft/
+exe alphacraft/controllers/
+exe alphacraft/controllers/splash/
+exe alphacraft/controllers/display/
+exe alphacraft/engine/optimiser/
+exe alphacraft/engine/resources/
